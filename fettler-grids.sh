a192b338317114194ab227063597c62a3c3058ec
Alex Wilson <alexwilsonphoto@gmail.com>
2017-02-19 16:36:24 -0500

    fix edges of soft grids

--- fettler-grids.sh
+++ fettler-grids.sh
@@ -30,6 +30,8 @@ PIXELS_MINUS_1=$(($PIXELS_PER_SQUARE_SIDE - 1))
 PIXELS_MINUS_2=$(($PIXELS_PER_SQUARE_SIDE - 2))
 PIXELS_10=$(($PIXELS_PER_SQUARE_SIDE/10))
 PIXELS_90=$(($PIXELS_PER_SQUARE_SIDE - PIXELS_10 - 1))
+PIXELS_PER_SQUARE_SIDE3=$(($PIXELS_PER_SQUARE_SIDE * 3))
+SQUARE_SIZE3="${PIXELS_PER_SQUARE_SIDE3}x${PIXELS_PER_SQUARE_SIDE3}"
 
 mkdir -p ${TMPDIR}
 mkdir -p ${GRIDDIR}
@@ -39,39 +41,45 @@ echo "Making grids..."
 magick -size ${CANVAS_SIZE} xc:grey50 ${GRIDDIR}/blank.png
 magick -size ${SQUARE_SIZE} xc:grey75 -fill grey50 -draw "rectangle 1,1 ${PIXELS_MINUS_2},${PIXELS_MINUS_2}" ${TMPDIR}/tile75.png
 magick -size ${SQUARE_SIZE} xc:grey60 -fill grey50 -draw "rectangle 1,1 ${PIXELS_MINUS_2},${PIXELS_MINUS_2}" ${TMPDIR}/tile60.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/tile75.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/tile75_soft.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/tile60.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/tile60_soft.png
 
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/tile75.png ${GRIDDIR}/grid_75_hard.png
-magick convert ${GRIDDIR}/grid_75_hard.png -blur 0x1 ${GRIDDIR}/grid_75_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/tile75_soft.png ${GRIDDIR}/grid_75_soft.png
 magick convert -alpha off ${GRIDDIR}/grid_75_hard.png -negate ${GRIDDIR}/grid_25_hard.png
-magick convert ${GRIDDIR}/grid_25_hard.png -blur 0x1 ${GRIDDIR}/grid_25_soft.png
+magick convert -alpha off ${GRIDDIR}/grid_75_soft.png -negate ${GRIDDIR}/grid_25_soft.png
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/tile60.png ${GRIDDIR}/grid_60_hard.png
-magick convert ${GRIDDIR}/grid_60_hard.png -blur 0x1 ${GRIDDIR}/grid_60_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/tile60_soft.png ${GRIDDIR}/grid_60_soft.png
 magick convert -alpha off ${GRIDDIR}/grid_60_hard.png -negate ${GRIDDIR}/grid_40_hard.png
-magick convert ${GRIDDIR}/grid_40_hard.png -blur 0x1 ${GRIDDIR}/grid_40_soft.png
+magick convert -alpha off ${GRIDDIR}/grid_60_soft.png -negate ${GRIDDIR}/grid_40_soft.png
 
-magick -size ${SQUARE_SIZE} xc:grey60 -fill grey50 -draw "rectangle 1,1 ${PIXELS_MINUS_2},${PIXELS_MINUS_2}" -fill grey50 -draw "rectangle 0,${PIXELS_10} ${PIXEL_COUNT},${PIXELS_90} rectangle ${PIXELS_10},0 ${PIXELS_90},${PIXEL_COUNT}" ${TMPDIR}/cross60.png
 magick -size ${SQUARE_SIZE} xc:grey75 -fill grey50 -draw "rectangle 1,1 ${PIXELS_MINUS_2},${PIXELS_MINUS_2}" -fill grey50 -draw "rectangle 0,${PIXELS_10} ${PIXEL_COUNT},${PIXELS_90} rectangle ${PIXELS_10},0 ${PIXELS_90},${PIXEL_COUNT}" ${TMPDIR}/cross75.png
+magick -size ${SQUARE_SIZE} xc:grey60 -fill grey50 -draw "rectangle 1,1 ${PIXELS_MINUS_2},${PIXELS_MINUS_2}" -fill grey50 -draw "rectangle 0,${PIXELS_10} ${PIXEL_COUNT},${PIXELS_90} rectangle ${PIXELS_10},0 ${PIXELS_90},${PIXEL_COUNT}" ${TMPDIR}/cross60.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/cross75.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/cross75_soft.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/cross60.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/cross60_soft.png
 
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/cross75.png ${GRIDDIR}/cross_75_hard.png
-magick convert ${GRIDDIR}/cross_75_hard.png -blur 0x1 ${GRIDDIR}/cross_75_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/cross75_soft.png ${GRIDDIR}/cross_75_soft.png
 magick convert -alpha off ${GRIDDIR}/cross_75_hard.png -negate ${GRIDDIR}/cross_25_hard.png
-magick convert ${GRIDDIR}/cross_25_hard.png -blur 0x1 ${GRIDDIR}/cross_25_soft.png
+magick convert -alpha off ${GRIDDIR}/cross_75_soft.png -negate ${GRIDDIR}/cross_25_soft.png
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/cross60.png ${GRIDDIR}/cross_60_hard.png
-magick convert ${GRIDDIR}/cross_60_hard.png -blur 0x1 ${GRIDDIR}/cross_60_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/cross60_soft.png ${GRIDDIR}/cross_60_soft.png
 magick convert -alpha off ${GRIDDIR}/cross_60_hard.png -negate ${GRIDDIR}/cross_40_hard.png
-magick convert ${GRIDDIR}/cross_40_hard.png -blur 0x1 ${GRIDDIR}/cross_40_soft.png
+magick convert -alpha off ${GRIDDIR}/cross_60_soft.png -negate ${GRIDDIR}/cross_40_soft.png
 
 magick -size ${SQUARE_SIZE} xc:grey50 -stroke grey75 -draw "line 0,0 0,${PIXELS_MINUS_1} line 0,0 ${PIXELS_MINUS_1},0" -stroke grey25 -draw "line 0,${PIXELS_MINUS_1}, ${PIXELS_MINUS_1},${PIXELS_MINUS_1} line ${PIXELS_MINUS_1},1 ${PIXELS_MINUS_1},${PIXELS_MINUS_1}" ${TMPDIR}/box75.png
 magick -size ${SQUARE_SIZE} xc:grey50 -stroke grey60 -draw "line 0,0 0,${PIXELS_MINUS_1} line 0,0 ${PIXELS_MINUS_1},0" -stroke grey40 -draw "line 0,${PIXELS_MINUS_1}, ${PIXELS_MINUS_1},${PIXELS_MINUS_1} line ${PIXELS_MINUS_1},1 ${PIXELS_MINUS_1},${PIXELS_MINUS_1}" ${TMPDIR}/box60.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/box75.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/box75_soft.png
+magick convert -size ${SQUARE_SIZE3} tile:${TMPDIR}/box60.png -blur 0x1 -gravity Center -crop ${SQUARE_SIZE}+0+0 +repage ${TMPDIR}/box60_soft.png
 
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/box75.png ${GRIDDIR}/box_75_hard.png
-magick convert ${GRIDDIR}/box_75_hard.png -blur 0x1 ${GRIDDIR}/box_75_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/box75_soft.png ${GRIDDIR}/box_75_soft.png
 magick convert -alpha off ${GRIDDIR}/box_75_hard.png -negate ${GRIDDIR}/box_25_hard.png
-magick convert ${GRIDDIR}/box_25_hard.png -blur 0x1 ${GRIDDIR}/box_25_soft.png
+magick convert -alpha off ${GRIDDIR}/box_75_soft.png -negate ${GRIDDIR}/box_25_soft.png
 magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/box60.png ${GRIDDIR}/box_60_hard.png
-magick convert ${GRIDDIR}/box_60_hard.png -blur 0x1 ${GRIDDIR}/box_60_soft.png
+magick convert -size ${CANVAS_SIZE} tile:${TMPDIR}/box60_soft.png ${GRIDDIR}/box_60_soft.png
 magick convert -alpha off ${GRIDDIR}/box_60_hard.png -negate ${GRIDDIR}/box_40_hard.png
-magick convert ${GRIDDIR}/box_40_hard.png -blur 0x1 ${GRIDDIR}/box_40_soft.png
+magick convert -alpha off ${GRIDDIR}/box_60_soft.png -negate ${GRIDDIR}/box_40_soft.png
 
 echo "... done."
 
